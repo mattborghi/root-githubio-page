ac2c8e0cf17a30d28dc26c2a9ee2b929c6f26c63
Matias Borghi <borghi.matias@gmail.com>
2020-05-25 16:24:13 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/header'
+
+const sections = [
+  { title: 'CV', url: 'https://mattborghi.github.io/CV' },
+  { title: 'Blog', url: 'https://mattborghi.github.io/blog' },
+  { title: 'Projects', url: '#' },
+  { title: 'French', url: '#' },
+  { title: 'Violin', url: '#' },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header title="Matias Borghi" sections={sections} />
+       {/* <header className="App-header"> */}
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -17,8 +27,8 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
+        </a> */}
+      {/* </header>  */}
     </div>
   );
 }
