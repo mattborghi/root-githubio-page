e542024d67ca47f54080d3da7d611179e890f8c2
Matias Borghi <borghi.matias@gmail.com>
2020-08-06 23:30:45 -0300

    updated main github page

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Header from './Components/header'
-
-const sections = [
-  { title: 'CV', url: 'https://mattborghi.github.io/CV' },
-  { title: 'Blog', url: 'https://mattborghi.github.io/blog' },
-  { title: 'Projects', url: '#' },
-  { title: 'French', url: '#' },
-  { title: 'Violin', url: '#' },
-];
+import React from "react";
+import "./App.css";
+import Header from './components/Header';
+import Data from './Data';
 
 function App() {
   return (
     <div className="App">
-      <Header title="Matias Borghi" sections={sections} />
-       {/* <header className="App-header"> */}
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      {/* </header>  */}
+      <Header resumeData={Data} />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import Snackbar from "@material-ui/core/Snackbar";
+
+export default function Header(props) {
+  const [alert, setAlert] = useState(false);
+  const { resumeData } = props;
+
+  return (
+    <React.Fragment>
+      <header id="home">
+        <nav id="nav-wrap">
+          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
+            Show navigation
+          </a>
+          <a className="mobile-btn" href="#" title="Hide navigation">
+            Hide navigation
+          </a>
+          <ul id="nav" className="nav">
+            <li className="current">
+              <a href="#home" alt="home">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="https://mattborghi.github.io/CV" alt="cv">
+                CV
+              </a>
+            </li>
+            <li>
+              <a href="https://mattborghi.github.io/blog" alt="blog">
+                Blog
+              </a>
+            </li>
+            <li>
+              <a href="#projects" alt="projects" onClick={() => setAlert(alert => !alert)}>
+                Projects
+              </a>
+            </li>
+            <li>
+              <a href="#french" alt="french" onClick={() => setAlert(alert => !alert)}>
+                French
+              </a>
+            </li>
+            <li>
+              <a href="#violin" alt="violin" onClick={() => setAlert(alert => !alert)}>
+                Violin
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="row banner">
+          <div className="banner-text">
+            <h2 className="responsive-headline"> Hello, my name is </h2>
+            <h1 className="responsive-headline"> {resumeData.name}.</h1>
+            <h3 style={{ color: "#fff", fontFamily: "sans-serif " }}>
+              I am a {resumeData.role}. {resumeData.roleDescription}
+            </h3>
+            <hr />
+            <ul className="social">
+              {resumeData.socialLinks &&
+                resumeData.socialLinks.map((item) => {
+                  return (
+                    <li key={item.name}>
+                      <a
+                        href={item.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <i className={item.className}></i>
+                      </a>
+                    </li>
+                  );
+                })}
+            </ul>
+          </div>
+        </div>
+      </header>
+
+      <Snackbar
+        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+        open={alert}
+        onClose={() => setAlert(alert => !alert)}
+        message="Page not yet implemented!"
+        key="snackbar"
+        autoHideDuration={5000}
+      />
+    </React.Fragment>
+  );
+  // }
+}
