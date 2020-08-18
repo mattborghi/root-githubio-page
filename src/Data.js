41ed3804d347654944fe9f0165043896f12e2889
Matias Borghi <borghi.matias@gmail.com>
2020-08-18 00:08:16 -0300

    clean up data file

--- src/Data.js
+++ src/Data.js
@@ -3,7 +3,6 @@ let Data = {
   name: "Matias Borghi",
   role: "Physicist currently working as a Quantitative Analyst",
   linkedinId: "mattborghi",
-  // skypeid: "Your skypeid",
   roleDescription:
     "I like solving challenges using algorithms and learning about new technologies. Nowadays, on my free time, I enjoy playing violin and learning french.",
   socialLinks: [
@@ -17,92 +16,7 @@ let Data = {
       url: "http://github.com/mattborghi",
       className: "fa fa-github",
     },
-    // {
-    //   name: "google",
-    //   url: "http://twitter.com/rbhatia46",
-    //   className: "fa fa-twitter",
-    // },
   ],
-  // aboutme:
-  //   "I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-  // address: "India",
-  // website: "https://rbhatia46.github.io",
-  // education: [
-  //   {
-  //     UniversityName: "The LNM Insitute of Information Technology",
-  //     specialization: "Some specialization",
-  //     MonthOfPassing: "Aug",
-  //     YearOfPassing: "2020",
-  //     Achievements: "Some Achievements",
-  //   },
-  //   {
-  //     UniversityName: "Some University",
-  //     specialization: "Some specialization",
-  //     MonthOfPassing: "Jan",
-  //     YearOfPassing: "2018",
-  //     Achievements: "Some Achievements",
-  //   },
-  // ],
-  // work: [
-  //   {
-  //     CompanyName: "Some Company",
-  //     specialization: "Some specialization",
-  //     MonthOfLeaving: "Jan",
-  //     YearOfLeaving: "2018",
-  //     Achievements: "Some Achievements",
-  //   },
-  //   {
-  //     CompanyName: "Some Company",
-  //     specialization: "Some specialization",
-  //     MonthOfLeaving: "Jan",
-  //     YearOfLeaving: "2018",
-  //     Achievements: "Some Achievements",
-  //   },
-  // ],
-  // skillsDescription: "Your skills here",
-  // skills: [
-  //   {
-  //     skillname: "HTML5",
-  //   },
-  //   {
-  //     skillname: "CSS",
-  //   },
-  //   {
-  //     skillname: "Reactjs",
-  //   },
-  // ],
-  // portfolio: [
-  //   {
-  //     name: "project1",
-  //     description: "mobileapp",
-  //     imgurl: "images/portfolio/phone.jpg",
-  //   },
-  //   {
-  //     name: "project2",
-  //     description: "mobileapp",
-  //     imgurl: "images/portfolio/project.jpg",
-  //   },
-  //   {
-  //     name: "project3",
-  //     description: "mobileapp",
-  //     imgurl: "images/portfolio/project2.png",
-  //   },
-  //   {
-  //     name: "project4",
-  //     description: "mobileapp",
-  //     imgurl: "images/portfolio/phone.jpg",
-  //   },
-  // ],
-  // testimonials: [
-  //   {
-  //     description: "This is a sample testimonial",
-  //     name: "Some technical guy",
-  //   },
-  //   {
-  //     description: "This is a sample testimonial",
-  //     name: "Some technical guy",
-  //   },
-  // ],
 };
 
 export default Data;
